617bef23fc6a3e1519b3556c76d686e3bfed7a72
Gethubsathvik <140134402+Gethubsathvik@users.noreply.github.com>
2026-08-30 13:12:24 +0530

    Update main.go

--- app/services/packet_service/main.go
+++ app/services/packet_service/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net"
 
-	pb "github.com/yourusername/spirit-realm/app/services/packet_service/proto"
+	pb "github.com/Gethubsathvik/Spirit_Realm/app/services/packet_service/proto"
 	"google.golang.org/grpc"
 )
 
